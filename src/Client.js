a655c0b5ba9329e4c0eacf77cc4504ab94c48b2d
fabio0107id <fabio0107idcraft@gmail.com>
2020-09-16 19:45:12 +0700

    fully remove any code

--- src/Client.js
+++ src/Client.js
@@ -1,8 +1,7 @@
 "use strict";
 
+const request = require("node-superfetch");
 const EventEmitter = require("events");
-const https = require("https");
-const qs = require("querystring");
 
 class SecretDevClient extends EventEmitter {
 	/**
@@ -19,98 +18,14 @@ class SecretDevClient extends EventEmitter {
 		this.client = client;
 
 		/**
-		 * Event that fires when has been authorize is successful to load
+		 * Authorization client with event for get notif is has been connect to api wrapper
 		 * @event authorize
-		 * @param {Object} authClient Object bots was authorize
+		 * @param {?Object} authClient this a api bots get authorize
 		 */
 
-		/**
-		 * Event that fires when authorize is fail to load
-		 * @event error
-		 * @param {NodeJS.Error} e This audit log for fail the load
-		 */
-
-		client.on("ready", async () => {
-			try {
-				const authClient = await this.authorizeClient();
-
-				this.emit("authorize", authClient);
-			} catch (e) {
-				this.emit("error", e);
-			}
+		client.on("ready", () => {
+			authorizeClient(client).then(authClient => this.emit("authorize", authClient));
 		});
-	}
-
-	/**
-	 * Create the request point
-	 * @param {String} method Https method for to the use api wrapper
-	 * @param {String} endpoint API endpoint for to the use api wrapper
-	 * @param {?Object} [data] Data to send with the requests
-	 * @private
-	 * @return {Promise<object>}
-	 */
-	_request(method, endpoint, data) {
-		return new Promise((resolve, reject) => {
-			const response = {
-				raw: "",
-				body: null,
-				status: null,
-				headers: null
-			};
-
-			const options = {
-				hostname: `api.secretdev.tech`,
-				endpoint: `/api/${endpoint}`,
-				method,
-				headers: {}
-			};
-
-			if (this.client) options.headers.authorizeClient = this.client;
-			else console.error(
-				"[secretdev.js] Warning: No provide someone for client packages."
-			);
-
-			if (data && method === "post") options.headers['content-type'] = "application/json";
-			if (data && method === "get") options.path += `?${qs.encode(data)}`;
-
-			const request = https.request(options, res => {
-				response.status = res.statusCode;
-				response.headers = res.headers;
-				response.ok = res.statusCode >= 200 && res.statusCode < 300;
-
-				res.on("data", chunk => response.raw += chunk);
-
-				res.on("end", () => {
-					response.body = res.headers['content-type'].includes("application/json") ? JSON.parse(response.raw) : response.raw;
-
-					if (response.ok) resolve(response);
-					else {
-						const error = new Error(`${res.statusCode} ${res.statusMessage}`);
-
-						Object.assign(error, response);
-
-						reject(error);
-					}
-				});
-			});
-
-			request.on("error", err => reject(err));
-
-			if (data && method === "post") request.write(JSON.stringify(data));
-
-			request.end();
-		});
-	}
-
-	/**
-	 * Authorization Client with package to connect api Wrapper
-	 * @private
-	 * @return {Promise<object>}
-	 */
-	async authorizeClient() {
-		const { body: res } = await this._request("post", "authorizeClient", this.client);
-
-		return res;
 	}
 
 	/**
@@ -122,20 +37,22 @@ class SecretDevClient extends EventEmitter {
 		if (!id && !this.client) throw new Error("[getBot] No provide someone for bot IDs.");
 		if (!id || isNaN(id)) id = this.client.user.id;
 
-		const { body: bots } = await this._request("get", `bots/${id}`);
+		try {
+			const { body } = await request.get(`https://api.secretdev.tech/api/bots/${id}`);
 
-		if (bots.error === "not_found") return {
-			error: { message: "This Bot is Not Found", code: 404 }
-		};
+			const result = {
+				botID: body.botID,
+				ownerID: body.ownerID,
+				prefix: body.prefix,
+				approve: body.approve
+			};
 
-		const result = {
-			botID: bots.botID,
-			ownerID: bots.ownerID,
-			prefix: bots.prefix,
-			approve: bots.approve
-		};
-
-		return result;
+			return result;
+		} catch (e) {
+			return {
+				error: { message: "This Bot is Not Found", code: 404 }
+			};
+		}
 	}
 
 	/**
@@ -146,11 +63,11 @@ class SecretDevClient extends EventEmitter {
 	 * @return {Promise<object>}
 	 */
 	async botsArray(obj = {}) {
-		const blockedTypeof = ["number", "string", "boolean"];
+		const blockType = ["number", "string", "boolean"];
 
-		if (blockedTypeof.includes(typeof obj)) throw new Error("[botsArray] No provide some with object structures.");
+		if (blockType.includes(typeof obj)) throw new Error("[botsArray] No provide some with object structures.");
 
-		const { body } = await this._request("get", "botsArray");
+		const { body } = await request.get("https://api.secretdev.tech/api/botsArray");
 
 		if (obj.ownerID !== undefined && obj.prefix !== undefined) {
 			if (!obj.ownerID || isNaN(obj.ownerID)) throw new Error("[botsArray] No provide someone for developer IDs.");
@@ -194,7 +111,7 @@ class SecretDevClient extends EventEmitter {
 	async getUser(id) {
 		if (!id || isNaN(id)) throw new Error("[getUser] No provide someone for discord IDs.");
 
-		const { body: botsArray } = await this._request("get", "botsArray");
+		const { body: botsArray } = await request.get("https://api.secretdev.tech/api/botsArray");
 
 		if (this.client.users.fetch !== undefined) {
 			const user = await this.client.users.fetch(id);
@@ -249,3 +166,23 @@ class SecretDevClient extends EventEmitter {
 }
 
 module.exports = SecretDevClient;
+
+/**
+ * Authorization client for catch include data
+ * @param {any} client This client package provide
+ * @return {Promise<object>}
+ */
+async function authorizeClient(client) {
+	const { body } = await request.get("https://api.secretdev.tech/api/botsArray");
+
+	const result = body.filter(bot => bot.botID.includes(client.user.id))[0];
+
+	if (result === undefined) throw new Error(
+		"[secretdev.js] Warning: this client is not Registered, please register and approve now!"
+	);
+	else if (result.approve !== undefined && !result.approve) throw new Error(
+			"[secretdev.js] Warning: this client is not Approve, please patient the Approve this client."
+		);
+
+	return result;
+}
